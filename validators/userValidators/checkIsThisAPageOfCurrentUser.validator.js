6c7c84f5ab30415b9c5029b5cb1d17dcf88239fb
DanyloKopytko <leonelluson@gmail.com>
2019-10-20 12:39:12 +0300

    Minor improvements

--- validators/userValidators/checkIsThisAPageOfCurrentUser.validator.js
+++ validators/userValidators/checkIsThisAPageOfCurrentUser.validator.js
@@ -1,6 +1,4 @@
 module.exports = (user_id, userIdFromToken) => {
-    console.log(user_id);
-    console.log(userIdFromToken);
     if (+user_id !== userIdFromToken) {
         throw new Error('This is not your page, invader');
     }
