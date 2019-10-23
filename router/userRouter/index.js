458cb877e13e289c52abd48bb015741b6a490ee3
DanyloKopytko <leonelluson@gmail.com>
2019-10-23 23:44:04 +0300

    Deleted useless router

--- router/userRouter/index.js
+++ router/userRouter/index.js
@@ -1,7 +1,5 @@
 const getUser = require('./user.router');
-const userRegister = require('./userRegister.router');
 
 module.exports = {
-  getUser,
-  userRegister
+  getUser
 };
--- router/userRouter/userRegister.router.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-
-let { renderUser } = require('../../render');
-
-router.get('/', renderUser.register);
-
-module.exports = router;
